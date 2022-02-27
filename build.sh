3878745843cc53959d1ccd46ff4b86aa6eae5673
Siva Mahadevan <me@svmhdvn.name>
2022-02-26 23:49:14 -0500

    work on 21.02.2 release instead of HEAD

--- build.sh
+++ build.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
-openwrt_url='https://git.openwrt.org/openwrt/openwrt.git'
-jobs=$(nproc)
-top="$PWD"
-if [ ! -d "$top/openwrt" ]; then
-    git clone "$openwrt_url" "$top/openwrt"
-fi
-cd "$top/openwrt" || exit 1
+set -e -x
 
-git reset --hard origin/master
-git pull
-make distclean
-git am "$top/patches/*.patch"
+openwrt_url='https://git.openwrt.org/openwrt/openwrt.git'
+openwrt_tag='v21.02.2'
+cpus=$(nproc)
+top="$PWD"
+
+if [ ! -d "$top/openwrt" ]; then
+    git clone "$openwrt_url" -b "$openwrt_tag" "$top/openwrt"
+fi
+cd "$top/openwrt"
+
+git reset --hard "$openwrt_tag"
+
+# TODO we only need to pull if we are building origin/master
+# git pull
+
+make dirclean
+git am $top/patches/*.patch
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 make menuconfig
-make download -j"$jobs"
-make -j"$jobs"
+make download -j"$cpus"
+make -j"$cpus"
