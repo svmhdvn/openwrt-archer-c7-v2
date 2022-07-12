94fc1b321ad69570b7f75ecb87e0c0c9f06ca879
Siva Mahadevan <me@svmhdvn.name>
2022-07-12 01:37:26 -0400

    working config

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 set -e -x
 
 openwrt_url='https://git.openwrt.org/openwrt/openwrt.git'
-openwrt_tag='v22.03.0-rc1'
+openwrt_tag='v22.03.0-rc5'
 
 # TODO parse this from within openwrt itself
-kernel_version='5.10.111'
+kernel_version='5.10.127'
 
 cpus=$(nproc)
 top="$PWD"
@@ -25,11 +25,12 @@ git reset --hard "$openwrt_tag"
 git am $top/patches/*.patch
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+make -j"$cpus" download
 make -j"$cpus" menuconfig
 make -j"$cpus" kernel_menuconfig CONFIG_TARGET=subtarget
 
 # set all kernel modules to be built-in
 sed -i -e "s/=m/=y/g" "build_dir/target-mips_74kc_musl/linux-ath79_generic/linux-$kernel_version/.config"
 
-make -j"$cpus" download
 make -j"$cpus"
+cp targets/ath79/generic/openwrt-ath79-generic-tplink_archer-c7-v2-squashfs-sysupgrade.bin "$top"
