10a0c6e2cdb892ed55baf4fc4d9ad03e23fea99b
Siva Mahadevan <me@svmhdvn.name>
2023-07-09 19:26:27 -0400

    refresh patches and update to v23.05.0-rc2

--- build.sh
+++ build.sh
@@ -3,17 +3,14 @@
 set -e -x
 
 openwrt_url='https://git.openwrt.org/openwrt/openwrt.git'
-openwrt_tag='v22.03.5'
+openwrt_tag='v23.05.0-rc2'
 #openwrt_tag='origin/master'
-
-# TODO parse this from within openwrt itself
-kernel_version='5.10.176'
 
 cpus=$(nproc)
 top="$PWD"
 
 if [ ! -d "$top/openwrt" ]; then
-    git clone "$openwrt_url" -b "$openwrt_tag" "$top/openwrt"
+    git clone --depth 1 "$openwrt_url" -b "$openwrt_tag" "$top/openwrt"
 fi
 cd "$top/openwrt"
 
@@ -31,7 +28,7 @@ make -j"$cpus" menuconfig
 make -j"$cpus" kernel_menuconfig CONFIG_TARGET=subtarget
 
 # set all kernel modules to be built-in
-sed -i -e "s/=m/=y/g" "build_dir/target-mips_74kc_musl/linux-ath79_generic/linux-$kernel_version/.config"
+find build_dir/target-mips_74kc_musl/linux-ath79_generic -name .config -type f | xargs sed -i -e "s/=m/=y/g"
 
 make -j"$cpus"
 cp bin/targets/ath79/generic/openwrt-ath79-generic-tplink_archer-c7-v2-squashfs-sysupgrade.bin "$top"
